8aa25670f45bb15ac25166530b6349a2bdae6bac
OlegZHL <olegZHL@yandex.ru>
2025-01-04 23:36:17 +0300

    first_solution

---
+++ Lesson_1/01.js
@@ -0,0 +1,20 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let sum = 0;
+let firstNumber = 1;
+let lastNumber = 20;
+
+while(firstNumber<=20){
+console.log(`Сумма чисел: ${sum = sum + firstNumber}`);
+
+if(firstNumber === 20){
+    console.log("Достигли результата!");
+} else {
+    console.log("Идём дальше!");
+}
+
+
+firstNumber++
+};
