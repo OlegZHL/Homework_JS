d9e9fdef1446197b2baa27560f6ddb702699a36b
OlegZHL <olegZHL@yandex.ru>
2025-01-25 17:38:40 +0300

    project

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -19,9 +19,10 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 
 let array1 = [1, 2, 3];
 let array2 = [2, 3, 4];
-let sharedArray =[];
+
 
 function findCommonElements(ar1, ar2) {
+    let sharedArray =[];
     
     for( let i = 0; i < ar1.length; i++){
         for( let j = 0; j < ar2.length; j++){
@@ -32,9 +33,9 @@ function findCommonElements(ar1, ar2) {
         }
         
     }
-    
+    return sharedArray;
 }
 
 findCommonElements(array1, array2);
 
-console.log(sharedArray);
+console.log(findCommonElements(array1, array2));
--- lesson-04/04.js
+++ lesson-04/04.js
@@ -11,5 +11,18 @@
 
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
+let str = "doubleEachCharacter";
 
-function doubleEachCharacter() {}
+function doubleEachCharacter(st) {
+    let intermediateValue ="";
+    let finalValue ="";
+    for( let i = 0; i < st.length; i++ ){
+        intermediateValue = st[i]+ st[i];
+        finalValue += intermediateValue;
+    }
+    return finalValue;
+}
+
+
+doubleEachCharacter(str);
+console.log(doubleEachCharacter(str));
