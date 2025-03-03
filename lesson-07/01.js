95fc5c073ba7a12c3bab2bcb8a7f8b6f3b8e0e29
OlegZHL <olegZHL@yandex.ru>
2025-03-03 23:34:34 +0300

    project

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -6,4 +6,18 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(phrase) {
+  let phrase1 = "";
+  for (let i = 0; i < phrase.length; i++) {
+    if (i === 0) {
+      phrase1 = phrase1 + phrase[i].toUpperCase();
+    } else if (phrase[i - 1] === " ") {
+      phrase1 = phrase1 + phrase[i].toUpperCase();
+    } else {
+      phrase1 = phrase1 + phrase[i];
+    }
+  }
+  return phrase1;
+}
+
+console.log(capitalizeWords("hello world from javascript"));
